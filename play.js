ecc56e5237051b9a029c514cb93c6cf051a6ffc6
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-04 21:42:29 +0000

    refactored play.js - seperate input.js fiel

--- play.js
+++ play.js
@@ -1,32 +1,6 @@
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-
-    stdin.on("data", handleUserInput)
-    return stdin;
-    }
-
-const handleUserInput = function(key) {
-    if (key === '\u0003') 
-    process.exit();
-}
-
-
-
-
-
-
-
-
-
 const{ connect } = require('./client');
+const{ setupInput } = require('./input')
 /**
  * Establishes connection with the game server
  */
@@ -34,3 +8,5 @@ const{ connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
