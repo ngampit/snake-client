5c230970d641047ed053a5cab5e2a6b8c15c27ce
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-04 20:59:43 +0000

    send name message to server

--- client.js
+++ client.js
@@ -1,14 +1,21 @@
 const net = require('net');
 const connect = function() {
     const conn = net.createConnection({ 
-      host: '135.23.222.131',
-      port: 50542
+      host: '10.0.2.15',
+      port: 50541
     });
     // interpret incoming data as text
     conn.setEncoding('utf8'); 
+    conn.on('connect', ()=> {
+        console.log("successfully connected to game server");
+    })
+    conn.on('connect', () => {
+        conn.write("Name:MS");
+    })
     conn.on('data', (data) => {
         console.log(data);
     })
+
   
     return conn;
   }
