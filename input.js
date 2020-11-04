b25a81c57addc109a2b1a8974f7ab77eb926bf84
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-04 23:35:05 +0000

    modiefied file to take w a s d input keys

--- input.js
+++ input.js
@@ -2,20 +2,45 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+let connection; 
+
+const setupInput = function(conn) {
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
     stdin.resume();
 
     stdin.on("data", handleUserInput)
+    connection = conn; 
     return stdin;
-    }
+   
+}
 
 const handleUserInput = function(key) {
-    if (key === '\u0003') 
-    process.exit();
+    if (key === '\u0003') {
+        process.exit(); 
+    }
+    // w
+    if (key === 'w') { 
+        connection.write("Move: up")
+    }
+    //A
+    if (key === 'a')  {
+        connection.write("Move: left")
+    }
+ 
+    //S
+    if (key === 's') {
+        connection.write("Move: down")
+    }
+
+    //D 
+    if (key === 'd') {
+        connection.write("Move: right")
+
+    }
 }
+
 
 module.exports = { setupInput };
 
